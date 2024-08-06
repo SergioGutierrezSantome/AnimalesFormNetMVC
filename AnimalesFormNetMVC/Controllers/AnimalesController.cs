4cd23eb530f1d8b4b69a207a365d3a36d26887e4
SergioGutierrezSantome <gutierrez.sergio.360@gmail.com>
2024-08-06 12:49:22 +0200

    Agregar archivos de proyecto.

---
+++ AnimalesFormNetMVC/Controllers/AnimalesController.cs
@@ -0,0 +1,29 @@
+﻿using AnimalesFormNetMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnimalesFormNetMVC.Controllers
+{
+    public class AnimalesController : Controller
+    {
+        // GET: Animales
+        public ActionResult Index()
+        {
+            List<Animal> ListaAnimal = new List<Animal>()
+            {
+                new Animal(){IdAnimal=1,nombre="Gato"},
+                new Animal(){IdAnimal=2,nombre="Perro"},
+                new Animal(){IdAnimal=3,nombre="Cocodrilo"},
+                new Animal(){IdAnimal=4,nombre="Pajaro"},
+                new Animal(){IdAnimal=5,nombre="Aguila"}
+            };
+            Random rnd = new Random();
+            int NumAleatorio = rnd.Next(0, 5);
+            ViewBag.Message = "Tu animal de la suerte del dia es " + ListaAnimal[NumAleatorio].nombre;
+            return View(ListaAnimal);
+        }
+    }
+}
---
+++ AnimalesFormNetMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AnimalesFormNetMVC.MvcApplication" Language="C#" %>
